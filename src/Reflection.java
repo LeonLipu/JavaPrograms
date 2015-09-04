b5b6655f5c1a81d422858c8233e9327c27d9ba10
leonlipu <lipusipupara@gmail.com>
2015-09-04 05:52:18 +0530

    arraylist is added

---
+++ src/Reflection.java
@@ -0,0 +1,15 @@
+import javax.rmi.CORBA.Util;
+
+public class Reflection {
+
+	public static void main(String args[]) {
+
+		System.out.println(Object.class.getName());
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
---
+++ src/collection/arraylist.java
@@ -0,0 +1,23 @@
+package collection;
+
+import java.util.ArrayList;
+
+public class arraylist {
+	
+	public static void main(String[] args) {
+	
+	
+ArrayList<String> li=new ArrayList<String>();
+	li.add("something");
+	li.add("hello");
+	li.add("hello");
+	li.add("hello1");
+	li.add("hello");
+	li.add("hello");
+System.out.println(li);	
+	
+	}
+	
+	
+
+}
