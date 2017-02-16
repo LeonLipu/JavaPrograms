5be3f007dc031eaeea93cac37240e7f4f60c090a
LeonLipu <lipusipupara@gmail.com>
2017-02-16 17:11:04 +0530

    mergesort fixed with comment

--- src/datastructrue/sorting/mergesort.java
+++ src/datastructrue/sorting/mergesort.java
@@ -1,5 +1,8 @@
 package datastructrue.sorting;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by brkar on 2/14/2017.
  */
@@ -9,39 +12,66 @@ public class mergesort {
 
     public static void main(String[] args) {
 
-     int arr[]={2,5,3,5,6,2};
-        devide(arr,0,arr.length);
+        Integer arr[]={2,12,5,3,7,6,10,22,11,99,1,9,8,2,4,6};
+        System.out.println(arr.length);
+        devide(arr,0,arr.length-1);
 
-        for (int i=0;i<arr.length;i++){
-            System.out.println(arr[i]);
-        }
+        System.out.println(Arrays.asList(arr));
+
+
     }
 
 
-    public static void merge(int [] arr,int mid ,int i,int j){
+    public static void merge(Integer [] arr,int mid ,int i,int j){
 
         int start =i;
         int last=j;
-        int narr[]=new int [j-i];
+        Integer narr[]=new Integer [j-i+1];
+
+         //  j=j+1;
          int k=0;
-        while (k<=narr.length){
+         int b =i;
+         int e=mid+1;
 
-            if(arr[i]>arr[j]){
-                narr[k++]=arr[j--];
-            }
-            else{
-                narr[k++]=arr[i++];
-            }
+      System.out.println("The range is "+i +" "+j+"  sub array "+Arrays.asList(arr).subList(i,j+1));
+
+      //  System.out.println("the element "+arr[j]);
+
+       System.out.println("first half is "+ Arrays.asList(arr).subList(i,mid+1)+" range "+i+" "+ mid);
+      System.out.println("second  half is "+ Arrays.asList(arr).subList(e,j+1)+" range "+e+" "+j);
+
+
+      //  System.out.println("the element  "+arr[b]+" "+arr[mid]);
+        //System.out.println("the element  "+arr[e]+" "+arr[j]);
+
+         while (b<=mid || e<=j)
+         {
+
+             if(b<=mid && e<=j){
+
+                 if(arr[b]>arr[e])
+                 {
+                     narr[k++]=arr[e++];
+                 }
+                 else{
+                     narr[k++]=arr[b++];
+                 }
+
+             }else if(b<=mid)
+                 narr[k++]=arr[b++];
+             else
+                 narr[k++]=arr[e++];
+         }
+
+        for(i=start;i<=last;i++){
+            arr[i]=narr[i-start];
         }
+      System.out.println("new  Array "+Arrays.asList(narr));
 
-        for(i=start;i<last;i++){
-            arr[i]=narr[start-i];
-        }
-
-
+        System.out.println("*************************");
 
 }
-    public static void devide (int [] arr,int i,int j){
+    public static void devide (Integer [] arr,int i,int j){
 
        if(j<=i)
            return ;
@@ -50,10 +80,6 @@ public class mergesort {
        devide(arr,i,mid);
        devide(arr,mid+1,j);
        merge(arr,mid,i,j);
-
-
-
-
 
     }
 
